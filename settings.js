f235b079956924f52467af974e6b202e17ad9a59
sidfakenews986 <bisonsicolo@hotmail.com>
2025-10-01 16:13:14 +0100

    feat: initial commit for settings.js

---
+++ settings.js
@@ -0,0 +1,14 @@
+let darkModeEnabled = false;
+
+function toggleDarkMode() {
+  darkModeEnabled = !darkModeEnabled;
+  const body = document.body;
+  body.classList.toggle('dark-mode', darkModeEnabled);
+}
+
+// Fügen Sie Ihre Zeitplanungslogik hier hinzu
+
+module.exports = {
+  toggleDarkMode,
+  isDarkModeEnabled: () => darkModeEnabled
+};
