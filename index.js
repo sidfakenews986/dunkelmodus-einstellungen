94d4e3295bd55576f7f89d8835bd858f8cbe280f
sidfakenews986 <bisonsicolo@hotmail.com>
2025-10-01 16:13:32 +0100

    Fix typo in German welcome message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Willkommen zu Dunkelmodus-Einstellungen!');
+  res.send('Willkommen zu den Dunkelmodus-Einstellungen!');
 });
 
 // Endpoint zum Aktivieren des Dunkelmodus
