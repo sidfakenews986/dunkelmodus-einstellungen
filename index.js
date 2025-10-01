e3a537dc41675981e9b2a7dbae2714995e400c87
sidfakenews986 <bisonsicolo@hotmail.com>
2025-10-01 16:13:08 +0100

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Willkommen zu Dunkelmodus-Einstellungen!');
+});
+
+// Endpoint zum Aktivieren des Dunkelmodus
+app.post('/toggle-dark-mode', (req, res) => {
+  const isEnabled = req.body.enabled;
+  // Logik zum Aktivieren/Deaktivieren des Dunkelmodus hier
+  res.json({ message: `Dunkelmodus ${isEnabled ? 'aktiviert' : 'deaktiviert'}` });
+});
+
+app.listen(port, () => {
+  console.log(`Server läuft auf http://localhost:${port}`);
+});
